566212fba272ec2226c6ac8320c193911d047c4f
Bongani005 <biqua005@gmail.com>
2025-06-15 16:13:00 +0200

    Structure

---
+++ src/main/java/com/spring_boot_api_csv_file/SpringBootApiCsvFileApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_api_csv_file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiCsvFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiCsvFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot_api_csv_file/SpringBootApiCsvFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_api_csv_file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiCsvFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
